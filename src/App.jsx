07d6bb513f0d4ca425e57f6ff195db5a94309d24
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-07-10 14:22:48 -0400

    Moved  the unordered list and the todos.map... to the return statement in TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
-  const todos = [
-    { id: 1, title: 'review resources' },
-    { id: 2, title: 'take notes' },
-    { id: 3, title: 'code out app' },
-  ];
-
   return (
     <>
       <h1>My Todos</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <TodoList></TodoList>
     </>
   );
 }
