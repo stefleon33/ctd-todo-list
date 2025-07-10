1dd4057e32421c9c38b2fce856a6de3d5b20bc0f
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-07-10 14:05:55 -0400

    Syntax updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   const todos = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-  ]
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ];
 
   return (
-    <div>
+    <>
       <h1>My Todos</h1>
       <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
       </ul>
-    </div>
+    </>
   );
 }
 
-export default App
+export default App;
