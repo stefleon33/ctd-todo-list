3e1289e63bc96efc90e346f35767dcbd604e0485
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-07-14 13:49:25 -0400

    useState imported and newTodo state created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('Example Text');
+
   return (
     <>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </>
   );
