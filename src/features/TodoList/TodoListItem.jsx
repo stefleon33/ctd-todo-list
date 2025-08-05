44fbdba7b05876353bd859ba90590d8328ca365d
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-08-05 17:01:46 -0400

    Allow Users To Edit Existing Todos

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,9 +1,42 @@
-function TodoListItem({ todo }) {
+import { useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+
+function TodoListItem({ todo, onCompleteTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  function handleEdit(event) {
+    setWorkingTitle(event.target.value);
+  }
+
+  function handleCancel() {
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  }
+
   return (
     <li>
-      <div>
-        <h2>{todo.title}</h2>
-      </div>
+      <form>
+        {isEditing ? (
+          <>
+            <TextInputWithLabel value={workingTitle} onChange={handleEdit} />
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </>
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.isCompleted}
+                onChange={() => onCompleteTodo(todo.id)}
+              />
+            </label>
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          </>
+        )}
+      </form>
     </li>
   );
 }
