3940c3f7f059d9c8ca937bf19aa026dd4f21ee95
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-08-20 17:17:40 -0400

    Load Todos from Airtable

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+      setErrorMessage('');
+
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: token,
+        },
+      };
+
+      try {
+        const resp = await fetch(url, options);
+
+        if (!resp.ok) {
+          throw new Error(`Request failed: ${resp.status} ${resp.statusText}`);
+        }
+        const { records } = await resp.json();
+
+        setTodoList(
+          records.map((record) => {
+            const todo = {
+              id: record.id,
+              title: record.fields?.title ?? '',
+              isCompleted: Boolean(record.fields?.isCompleted),
+            };
+            return todo;
+          })
+        );
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
 
   function addTodo(title) {
     const newTodo = {
@@ -43,6 +87,7 @@ function App() {
         todoList={todoList}
         onUpdateTodo={updateTodo}
         onCompleteTodo={completeTodo}
+        isLoading={isLoading}
       />
     </>
   );
